592603a8c1691e65d34383d58a8ffaf4e7b264ed
jfsfdr <32168926+jfsfdr@users.noreply.github.com>
2017-11-09 17:38:22 +0000

    Add files via upload

---
+++ ÁreaDoTrapezio.java
@@ -0,0 +1,25 @@
+package área.pkgdo.trapezio;
+/**
+ * Autor: Jaime Figueiredo
+ * Data 09/11/2017
+ */
+import java.util.Scanner;
+public class ÁreaDoTrapezio {
+ public static void main(String[] args) {
+        //Variáveis
+        double bm,bM,h,a;
+        Scanner dados =new Scanner(System.in);
+        // Código Principal
+        System.out.print("Qual é a base menor ? ");
+        bm=dados.nextDouble();
+        
+        System.out.print("Qual é a base maior ? ");
+        bM=dados.nextDouble();
+        
+        System.out.print("Qual é a altura ? ");
+        h=dados.nextDouble();
+        
+        a=((bm+bM)/2)*h;
+        System.out.print("A área é " + a);
+ }
+}
