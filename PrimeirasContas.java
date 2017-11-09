286bc10e31f26bc9655a9a6b15129637c879ced0
jfsfdr <32168926+jfsfdr@users.noreply.github.com>
2017-11-09 17:21:53 +0000

    Add files via upload

---
+++ PrimeirasContas.java
@@ -0,0 +1,12 @@
+package primeiras.contas;
+//Autor: Jaime Figueiredo
+//Data: 09/11/2017
+public class PrimeirasContas {
+    public static void main(String[] args) {
+        //Codigo Base
+		System.out.printf("%d\n",4+3*(15-2));
+		System.out.printf("%f\n", 40.0/9.0);
+		System.out.printf("%d", 40%3);
+	}
+    
+}
