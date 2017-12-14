88085ff7456c2916f0f021646c34a01f12c90ec5
jfsfdr <32168926+jfsfdr@users.noreply.github.com>
2017-12-14 17:33:48 +0000

    Add files via upload

---
+++ MultiplicaçaoRussa.java
@@ -0,0 +1,26 @@
+package multiplicaçaorussa;
+/**
+ * Jaime Figueiredo 
+ * 14/12/2017
+ */
+import java.util.Scanner;
+public class MultiplicaçaoRussa {
+    public static void main(String[] args) {
+        //Variaveis
+        int multiplicador , multiplicando , resultado ;
+        Scanner dados=new Scanner(System.in);
+        //Código Principal
+        System.out.print("Qual é o multiplicando ? ");
+        multiplicando=dados.nextInt();
+        System.out.print("Qual é o multiplicador ? ");
+        multiplicador=dados.nextInt();
+        //Mostrar Leitura
+        System.out.print("O multiplicando é " + multiplicando + "\n");
+        System.out.print("O multiplicador é " + multiplicador);
+        //While
+        
+        
+        
+    }
+    
+}
